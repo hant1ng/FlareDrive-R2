6039c58319bdcf693b55a6e75022adddf7ddacf2
日他妈叼 <134476776+hant1ng@users.noreply.github.com>
2026-02-24 03:47:45 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -18,7 +18,7 @@
     <div class="app-bar">
       <a class="app-title-container" style="display: flex; align-items: center;" href="/">
         <img src="/assets/homescreen.png" alt="FlareDrive" style="height: 24px" />
-        <h1 class="app-title" style="font-size: 20px;margin: 0 25px 0 8px; user-select: none;">FlareDrive</h1>
+        <h1 class="app-title" style="font-size: 20px;margin: 0 25px 0 8px; user-select: none;">Drive</h1>
       </a>
 
       <input type="search" v-model="search" aria-label="Search" placeholder="搜索" />
@@ -603,8 +603,8 @@ export default {
           window.history.pushState(null, "", url.toString());
         }
         document.title = this.cwd.replace(/.*\/(?!$)|\//g, "") === "/" 
-            ? "FlareDrive-R2 - 优雅的 Cloudflare R2 网盘文件库"
-            :`${this.cwd.replace(/.*\/(?!$)|\//g, "") || "/" } - 优雅的 Cloudflare R2 网盘文件库`;
+            ? "Hant 的网盘"
+            :`${this.cwd.replace(/.*\/(?!$)|\//g, "") || "/" } - Hant 的网盘`;
       },
       immediate: true,
     },
